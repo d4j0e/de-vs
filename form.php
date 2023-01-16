7155619bf78adbbf336256462bc738179ad47bef
d4j0e <122789039+d4j0e@users.noreply.github.com>
2023-01-16 12:03:50 +0100

    Add files via upload

---
+++ form.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+	<meta charset="UTF-8">
+	<title>VS Database</title>
+	<meta name="description" content="Kurzbeschreibung">
+	<link href="design.css" rel="stylesheet">
+</head>
+
+<body>
+
+<form action="auslesen.php" method="post">
+ <p>System: <input type="text" name="sys" /></p>
+ <p>Fundstücke <input type="textarea" name="inputvs" /></p>
+ <p><input type="eintragen" /></p>
+</form>
+
+<?php
+
+echo htmlspecialchars($_POST['sys']); ?>.
+
+echo "\n";
+
+$arr = explode("\r\n", trim($_POST['inputvs']));
+ 
+for ($i = 0; $i < count($arr); $i++) {
+   $zeile = $arr[$i];
+   echo $zeile;
+   echo "\n";
+}
+
+?>
+
+
+</body>
+</html>
